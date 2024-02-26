c8310ce5076158f9f341041d2609608010b1bb9e
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2024-02-26 15:18:41 -0600

    Replaced old code with code accessing a real API

--- amplify/backend/function/coinfunction/src/app.js
+++ amplify/backend/function/coinfunction/src/app.js
@@ -32,14 +32,23 @@ app.use(function(req, res, next) {
  * Coin GET method *
  ***************************/
 
-// Based on the textbook code
+// Based on code from Full Stack Serverless
 app.get('/coins', (req, res) => {
-  const coins = [
-    { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
-    { name: 'Etherium', symbol: 'ETH', price_usd: "400" },
-    { name: 'LiteCoin', symbol: 'LTC', price_usd: "150"}
-  ];
-  res.json({coins});  
+  // Define base URL
+  let apiUrl = 'https://api.coinlore.com/api/tickers?start=0&limit=10';
+
+  // Check if there are any query string parameters
+  // If so, reset the base url to include them
+  if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+    const { start = 0, limit = 10 } = req.apiGateway.event.queryStringParameters
+    apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`
+  }
+
+  axios.get(apiUrl)
+    .then(response => {
+      res.json({  coins: response.data.data })
+    })
+    .catch(err => res.json({error: err}));
 });
 
 /****************************
